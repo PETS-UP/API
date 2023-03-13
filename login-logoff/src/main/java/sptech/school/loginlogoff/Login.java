c89444ce52336560890e48671765c30ddc958279
Matheus Ferraz <99892467+Matheus-FOliveira@users.noreply.github.com>
2023-03-13 20:41:26 -0300

    Login-logoff
    Código básico de login e logoff para primeira sprint

---
+++ login-logoff/src/main/java/sptech/school/loginlogoff/Login.java
@@ -0,0 +1,27 @@
+package sptech.school.loginlogoff;
+
+public class Login {
+    private String usuario;
+    private String senha;
+
+    public Login(String usuario, String senha) {
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
