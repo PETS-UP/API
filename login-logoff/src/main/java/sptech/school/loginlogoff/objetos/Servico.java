a91e9216816f55dba01c830d7696e930a2a6346d
MauricioMSoares <mauricio.soares@sptech.school>
2023-04-01 02:21:08 -0300

    feat: criação de objetos iniciais

---
+++ login-logoff/src/main/java/sptech/school/loginlogoff/objetos/Servico.java
@@ -0,0 +1,51 @@
+package sptech.school.loginlogoff.objetos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Servico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    private String descricao;
+
+    @NotNull
+    @DecimalMin("0.0")
+    private Double preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
