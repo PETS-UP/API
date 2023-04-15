9434d588a0923f7f3c6f341997a28235384977d3
Matheus Ferraz <matheus.foliveira@sptech.school>
2023-04-15 18:49:41 -0300

    Crud de Usuario criado

--- src/main/java/com/petsup/api/repositories/UsuarioRepository.java
+++ src/main/java/com/petsup/api/repositories/UsuarioRepository.java
@@ -1,8 +1,12 @@
 package com.petsup.api.repositories;
 
-import com.petsup.api.entities.Usuario;
+import com.petsup.api.entities.usurario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    
+    Optional<Usuario> findById(Integer id);
+    void deleteById(Integer id);
+    Optional<Usuario> findByNome(String nome);
 }
