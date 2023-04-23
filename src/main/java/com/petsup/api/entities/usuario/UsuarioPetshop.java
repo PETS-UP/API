597ce6b04c1a1bc00b5f5aad3d5cde65a4ceb00d
MauricioMSoares <mauricio.soares@sptech.school>
2023-04-23 12:34:46 -0300

    fix: ajustes de cadastro de petshop

--- src/main/java/com/petsup/api/entities/usuario/UsuarioPetshop.java
+++ src/main/java/com/petsup/api/entities/usuario/UsuarioPetshop.java
@@ -20,7 +20,6 @@ public class UsuarioPetshop extends Usuario {
     @Size(min = 6, max = 100)
     private String razaoSocial;
 
-    @CNPJ
     private String CNPJ;
 
     @OneToMany(mappedBy = "fk_petshop", fetch = FetchType.LAZY)
