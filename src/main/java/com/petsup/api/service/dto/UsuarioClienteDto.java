ec13186825473845673db3b2cc106b0c6c361534
MauricioMSoares <mauricio.soares@sptech.school>
2023-04-23 21:10:11 -0300

    feat: mapeamento UsuarioClienteDTO

--- src/main/java/com/petsup/api/service/dto/UsuarioClienteDto.java
+++ src/main/java/com/petsup/api/service/dto/UsuarioClienteDto.java
@@ -1,17 +1,35 @@
 package com.petsup.api.service.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.br.CPF;
 
-public class UsuarioClienteDto extends UsuarioDto{
-    
-    @CPF
-    private String CPF;
+import java.time.LocalDate;
 
-    public String getCPF() {
-        return CPF;
+public class UsuarioClienteDto extends UsuarioDto{
+
+    @Schema(description = "Data de nascimento do cliente", example = "1999-01-01")
+    @Past
+    private LocalDate dataNasc;
+
+    @Schema(name = "CPF", description = "CPF do cliente", example = "12345678901")
+    @CPF
+    private String cpf;
+
+    public LocalDate getDataNasc() {
+        return dataNasc;
     }
 
-    public void setCPF(String CPF) {
-        this.CPF = CPF;
+    public void setDataNasc(LocalDate dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 }
