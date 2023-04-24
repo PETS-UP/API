128c10845725936a7e8be133a05fb5b33869339a
Matheus Ferraz <matheus.foliveira@sptech.school>
2023-04-24 17:30:15 -0300

    Correção simples

--- src/main/java/com/petsup/api/service/autentication/dto/PetshopDeatlhesDto.java
+++ src/main/java/com/petsup/api/service/autentication/dto/PetshopDeatlhesDto.java
@@ -22,7 +22,7 @@ public class PetshopDeatlhesDto implements UserDetails {
         this.senha = usuarioPetshop.getSenha();
         this.cep = usuarioPetshop.getCep();
         this.telefone = usuarioPetshop.getTelefone();
-        this.CNPJ = usuarioPetshop.getCNPJ();
+        this.CNPJ = usuarioPetshop.getCnpj();
         this.razaoSocial = usuarioPetshop.getRazaoSocial();
     }
 
