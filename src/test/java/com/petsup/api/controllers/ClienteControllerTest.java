4ddc051b358d7cf75169d00b5601a8047a3ca312
MauricioMSoares <mauricio.soares@sptech.school>
2023-05-20 15:31:53 -0300

    feat: implementado MockMvc para testes de controllers

--- src/test/java/com/petsup/api/controllers/ClienteControllerTest.java
+++ src/test/java/com/petsup/api/controllers/ClienteControllerTest.java
@@ -5,14 +5,20 @@ import com.petsup.api.service.dto.UsuarioClienteDto;
 import com.petsup.api.repositories.ClienteRepository;
 import com.petsup.api.service.UsuarioService;
 import com.petsup.api.builder.UsuarioClienteBuilder;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Optional;
 import java.util.List;
@@ -20,6 +26,9 @@ import java.util.List;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.RequestEntity.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 public class ClienteControllerTest {
@@ -33,17 +42,26 @@ public class ClienteControllerTest {
     @InjectMocks
     private ClienteController clienteController;
 
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(clienteController).build();
+    }
+
     @Test
-    void getUserById_RetornaClienteDeId1() {
-        Optional<UsuarioCliente> usuarioClienteOptional = Optional.of(UsuarioClienteBuilder.buildUsuarioCliente());
-//        ResponseEntity<UsuarioCliente> response = ResponseEntity.ok(usuarioClienteOptional.get());
+    void getUserById_RetornaClienteDeId1() throws Exception {
 
-        Mockito.when(clienteRepository.findById(any())).thenReturn(usuarioClienteOptional);
-//        Mockito.when(clienteController.getUserById(any())).thenReturn(response);
+        Integer id = 1;
+        UsuarioCliente usuarioCliente = UsuarioClienteBuilder.buildUsuarioCliente();
 
-        assertEquals(usuarioClienteOptional, clienteRepository.findById(1));
-        assertEquals(1, usuarioClienteOptional.get().getId());
-//        assertEquals(response, clienteController.getUserById(1));
+        when(clienteRepository.findById(Mockito.any())).thenReturn(Optional.of(usuarioCliente));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/clientes/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(usuarioCliente.getId()));
+
+        assertEquals(usuarioCliente.getId(), clienteRepository.findById(1).get().getId());
     }
 
     @Test
