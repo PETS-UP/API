a65cc2afcb9c84cafcd958e97b43aa156a575316
MauricioMSoares <mauricio.soares@sptech.school>
2023-06-10 14:03:41 -0300

    feat: agendamentoRespostaDto adicionado com retornos em String

--- src/main/java/com/petsup/api/service/dto/AgendamentoMapper.java
+++ src/main/java/com/petsup/api/service/dto/AgendamentoMapper.java
@@ -1,7 +1,10 @@
 package com.petsup.api.service.dto;
 
 import com.petsup.api.entities.Agendamento;
-import com.petsup.api.entities.Pet;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AgendamentoMapper {
     public static AgendamentoDto ofAgendamentoDto(Agendamento agendamento) {
@@ -20,5 +23,31 @@ public class AgendamentoMapper {
         agendamentoDto.setPreco(agendamento.getFkServico().getPreco());
 
         return agendamentoDto;
+    }
+
+    public static AgendamentoRespostaDto ofAgendamentoRespostaDto(Agendamento agendamento) {
+        AgendamentoRespostaDto agendamentoRespostaDto = new AgendamentoRespostaDto();
+
+        agendamentoRespostaDto.setDataHora(agendamento.getDataHora().format(DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss")));
+        agendamentoRespostaDto.setNomeCliente(agendamento.getFkCliente().getNome());
+        agendamentoRespostaDto.setNomePet(agendamento.getFkPet().getNome());
+        agendamentoRespostaDto.setNomePet(agendamento.getFkPet().getNome());
+        agendamentoRespostaDto.setEspecie(agendamento.getFkPet().getEspecie().toString());
+        agendamentoRespostaDto.setRaca(agendamento.getFkPet().getRaca().toString());
+        agendamentoRespostaDto.setSexo(agendamento.getFkPet().getSexo());
+        agendamentoRespostaDto.setServico(agendamento.getFkServico().getNome().toString());
+        agendamentoRespostaDto.setPreco(agendamento.getFkServico().getPreco().toString());
+
+        return agendamentoRespostaDto;
+    }
+
+    public static List<AgendamentoRespostaDto> ofListaAgendamentoRespostaDto(List<Agendamento> agendamentos) {
+        List<AgendamentoRespostaDto> listaAgendamentosDto = new ArrayList<>();
+
+        for (int i = 0; i < agendamentos.size(); i++) {
+            listaAgendamentosDto.add(ofAgendamentoRespostaDto(agendamentos.get(i)));
+        }
+
+        return listaAgendamentosDto;
     }
 }
