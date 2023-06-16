80f00900de5be0d78aa84c973a1c44d2912d33b4
MauricioMSoares <mauricio.soares@sptech.school>
2023-06-16 20:14:09 -0300

    feat: tratamento de dados para a dashboard 1

--- src/main/java/com/petsup/api/service/DashboardService.java
+++ src/main/java/com/petsup/api/service/DashboardService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class DashboardService {
@@ -14,20 +16,20 @@ public class DashboardService {
     @Autowired
     private AgendamentoRepository agendamentoRepository;
 
-//    public List<Integer> getAgendamentosUltimaSemana() {
-//        List<Agendamento> agendamentos = agendamentoRepository.findAllByDataHoraBetween(
-//                LocalDateTime.now().minusDays(7L), LocalDateTime.now()
-//        );
-//        List<Integer> qtdAgendamentos;
-//        int contador;
-//
-//        for (int i = 0; i < agendamentos.size(); i++) {
-//            Long toLong = (long) i;
-//        }
-//        agendamentos.stream().filter(agendamento -> agendamento.getDataHora().isEqual(LocalDateTime.now().minusDays(7L)))
-//
-//        return agendamentoRepository.findCountAgendamentoByDataHoraBetween(
-//                LocalDateTime.now().minusDays(7L), LocalDateTime.now()
-//        );
-//    }
+    public List<Integer> getAgendamentosUltimaSemana() {
+        List<Agendamento> agendamentos = agendamentoRepository.findAllByDataHoraBetween(
+                LocalDateTime.now().minusDays(7L), LocalDateTime.now()
+        );
+        List<Integer> qtdAgendamentos = new ArrayList<>();
+
+        for (int i = 1; i < 8; i++) {
+            Long toLong = (long) i;
+            List<Agendamento> aux =
+            agendamentos.stream().filter(agendamento -> agendamento.getDataHora().isEqual(LocalDateTime.now().minusDays(toLong)))
+                    .collect(Collectors.toList());
+            qtdAgendamentos.add(aux.size());
+        }
+
+        return qtdAgendamentos;
+    }
 }
