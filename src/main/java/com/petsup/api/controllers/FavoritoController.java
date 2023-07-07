546082d147b0a112ad779322cb2edd3d4e95f702
belthenrique <henrique.beltrao@sptech.school>
2023-07-07 17:06:48 -0300

    Métodos FavoritoController para FavoritoService

--- src/main/java/com/petsup/api/controllers/FavoritoController.java
+++ src/main/java/com/petsup/api/controllers/FavoritoController.java
@@ -4,115 +4,55 @@ import com.petsup.api.models.Favorito;
 import com.petsup.api.models.cliente.UsuarioCliente;
 import com.petsup.api.models.petshop.UsuarioPetshop;
 import com.petsup.api.repositories.cliente.ClienteRepository;
-import com.petsup.api.repositories.FavoritoRepository;
 import com.petsup.api.repositories.petshop.PetshopRepository;
-import com.petsup.api.mapper.UsuarioMapper;
 import com.petsup.api.dto.petshop.UsuarioPetshopDto;
+import com.petsup.api.services.FavoritoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+/*
+ POST:   /favoritos/{idCliente}/{idPetshop}
+ GET:    /favoritos/{idCliente}
+ GET:    /favoritos/{idCliente}/favoritado/{idPetshop}
+ DELETE: /favoritos/{idCliente}/{idPetshop}
+*/
 
 @RestController
 @RequestMapping("/favoritos/{idCliente}")
 public class FavoritoController {
 
     @Autowired
-    private ClienteRepository clienteRepository;
+    private FavoritoService favoritoService;
 
-    @Autowired
-    private PetshopRepository petshopRepository;
-
-    @Autowired
-    private FavoritoRepository favoritoRepository;
+    @PostMapping("/{idPetshop}")
+    public ResponseEntity<Void> postFavorito(@PathVariable Integer idCliente, @PathVariable Integer idPetshop){
+        favoritoService.postFavorito(idCliente, idPetshop);
+        return ResponseEntity.ok().build();
+    }
 
     @GetMapping
-    public ResponseEntity<List<UsuarioPetshopDto>> listarFavoritos(@PathVariable Integer idCliente){
-        Optional<UsuarioCliente> usuarioCliente = clienteRepository.findById(idCliente);
+    public ResponseEntity<List<UsuarioPetshopDto>> getFavoritos(@PathVariable Integer idCliente){
+        List<UsuarioPetshopDto> usuarioPetshopDtos = favoritoService.getFavoritos(idCliente);
 
-        if (usuarioCliente.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-
-        List<Favorito> favoritos = favoritoRepository.findAllByFkClienteId(idCliente);
-        List<UsuarioPetshop> petshops = new ArrayList<>();
-        List<UsuarioPetshopDto> petshopDetalhesDtos = new ArrayList<>();
-
-        if (favoritos.isEmpty()){
+        if (usuarioPetshopDtos.isEmpty()){
             return ResponseEntity.noContent().build();
         }
 
-        for (int i = 0; i < favoritos.size(); i ++){
-            petshops.add(favoritos.get(i).getFkPetshop());
-            petshopDetalhesDtos.add(UsuarioMapper.ofPetshopDto(petshops.get(i)));
-        }
-
-
-        return ResponseEntity.ok(petshopDetalhesDtos);
+        return ResponseEntity.ok(usuarioPetshopDtos);
     }
 
     @GetMapping("/favoritado/{idPetshop}")
     public ResponseEntity<Boolean> isFavoritado(@PathVariable Integer idCliente, @PathVariable Integer idPetshop){
-
-        Optional<UsuarioCliente> usuarioCliente = clienteRepository.findById(idCliente);
-        Optional<UsuarioPetshop> usuarioPetshop = petshopRepository.findById(idPetshop);
-
-        if (usuarioCliente.isEmpty()){
-            return ResponseEntity.notFound().build();
-        } else if(usuarioPetshop.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-
-        Optional<Favorito> favoritoOptional = favoritoRepository.findByFkClienteIdAndFkPetshopId(idCliente,
-                idPetshop);
-
-        if (favoritoOptional.isEmpty()){
-            return ResponseEntity.ok(false);
-        }
-
-        return ResponseEntity.ok(true);
-    }
-
-    @PostMapping("/{idPetshop}")
-    public ResponseEntity<Void> favoritar(@PathVariable Integer idCliente, @PathVariable Integer idPetshop){
-        Optional<UsuarioCliente> usuarioCliente = clienteRepository.findById(idCliente);
-        Optional<UsuarioPetshop> usuarioPetshop = petshopRepository.findById(idPetshop);
-
-        if (usuarioCliente.isEmpty()){
-            return ResponseEntity.notFound().build();
-        } else if(usuarioPetshop.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-
-        Favorito favorito = new Favorito();
-
-        favorito.setFkCliente(usuarioCliente.get());
-        favorito.setFkPetshop(usuarioPetshop.get());
-
-        favoritoRepository.save(favorito);
-
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(favoritoService.isFavoritado(idCliente, idPetshop));
     }
 
     @DeleteMapping("/{idPetshop}")
-    public ResponseEntity<Void> deletarFavorito(@PathVariable Integer idCliente, @PathVariable Integer idPetshop){
-        Optional<UsuarioCliente> usuarioCliente = clienteRepository.findById(idCliente);
-        Optional<UsuarioPetshop> usuarioPetshop = petshopRepository.findById(idPetshop);
-
-        if (usuarioCliente.isEmpty()){
-            return ResponseEntity.notFound().build();
-        } else if(usuarioPetshop.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-
-        Optional<Favorito> optionalFavorito = favoritoRepository.findByFkClienteIdAndFkPetshopId(idCliente, idPetshop);
-        Favorito favorito = optionalFavorito.get();
-
-        favoritoRepository.delete(favorito);
-
+    public ResponseEntity<Void> deleteFavorito(@PathVariable Integer idCliente, @PathVariable Integer idPetshop){
+        favoritoService.deleteFavorito(idCliente, idPetshop);
         return ResponseEntity.noContent().build();
     }
 }
