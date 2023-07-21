db202fea1832ce8ebca17d94553a3c73893b432d
Mauricio Massayuki <99891691+MauricioMSoares@users.noreply.github.com>
2023-07-21 16:55:14 -0300

    teste bean cors

--- src/main/java/com/petsup/api/ApiApplication.java
+++ src/main/java/com/petsup/api/ApiApplication.java
@@ -3,7 +3,10 @@ package com.petsup.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableScheduling
 @EnableFeignClients
@@ -12,6 +15,18 @@ public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("*")
+						.allowedOrigins("*")
+						.maxAge(3600);
+			}
+		};
 	}
 
 }
