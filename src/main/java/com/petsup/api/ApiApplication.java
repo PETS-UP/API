209cd6cb5896af2672a3be54883b7d3e71bd12cc
Mauricio Massayuki <99891691+MauricioMSoares@users.noreply.github.com>
2023-07-21 17:11:54 -0300

    teste cors 2

--- src/main/java/com/petsup/api/ApiApplication.java
+++ src/main/java/com/petsup/api/ApiApplication.java
@@ -22,7 +22,7 @@ public class ApiApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("*")
+				registry.addMapping("/**")
 						.allowedOrigins("*")
 						.maxAge(3600);
 			}
