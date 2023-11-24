0591d421cd1efee7adcae3f8fd4d6e8d2b609184
Gabriel Costa <gabriel.costa@vr.com.br>
2023-11-23 22:10:15 -0300

    atualizado script iniciar_jar

---
+++ iniciar_jar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Caminho para o arquivo JAR
+caminho_jar="/home/ubuntu/app-petsup-banco-prd.jar"
+
+# Verifica se o arquivo JAR existe
+if [ -f "$caminho_jar" ]; then
+    # Executa o arquivo JAR
+    java -jar "$caminho_jar &"
+else
+    echo "O arquivo JAR não foi encontrado em: $caminho_jar"
+fi
